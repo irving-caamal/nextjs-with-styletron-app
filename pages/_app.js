605355b1a219ea9bc323afd54940d0eb6de86ac6
Irving Caamal <Irvv.17@gmail.com>
2020-11-23 20:31:03 -0500

    api integration

--- pages/_app.js
+++ pages/_app.js
@@ -6,241 +6,237 @@ LICENSE file in the root directory of this source tree.
 
 /* eslint-env browser */
 
-// @flow
-import * as React from 'react';
+import * as React from "react";
 import {
-    createThemedStyled,
-    createThemedUseStyletron,
-    createThemedWithStyle,
-    BaseProvider,
-    DarkTheme,
-    DarkThemeMove,
-    LightTheme,
-    LightThemeMove,
-} from 'baseui';
+  createThemedStyled,
+  createThemedUseStyletron,
+  createThemedWithStyle,
+  BaseProvider,
+  DarkTheme,
+  DarkThemeMove,
+  LightTheme,
+  LightThemeMove,
+} from "baseui";
 
+import App from "next/app";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Block } from "baseui/block";
+import Router from "next/router";
+import { appWithTranslation } from "../i18n";
 
-import App from 'next/app';
-import {Provider as StyletronProvider} from 'styletron-react';
-import {Block} from 'baseui/block';
-import Router from 'next/router';
-import { appWithTranslation } from '../i18n'
-
-
-import {styletron, debug} from '../helpers/styletron';
+import { styletron, debug } from "../helpers/styletron";
 // $FlowFixMe
 //import {trackPageView} from '../helpers/ga';
-import DirectionContext from '../components/direction-context';
+import DirectionContext from "../components/direction-context";
 
 const breakpoints = {
-    small: 670,
-    medium: 920,
-    large: 1280,
+  small: 670,
+  medium: 920,
+  large: 1280,
 };
 
-    const ResponsiveTheme = Object.keys(breakpoints).reduce(
-    (acc, key) => {
-        acc.mediaQuery[
-            key
-            ] = `@media screen and (min-width: ${breakpoints[key]}px)`;
-        return acc;
-    },
-    {
-        breakpoints,
-        mediaQuery: {},
-    },
+const ResponsiveTheme = Object.keys(breakpoints).reduce(
+  (acc, key) => {
+    acc.mediaQuery[
+      key
+    ] = `@media screen and (min-width: ${breakpoints[key]}px)`;
+    return acc;
+  },
+  {
+    breakpoints,
+    mediaQuery: {},
+  }
 );
 
 const themes = {
-    LightTheme: {...LightTheme, ...ResponsiveTheme},
-    LightThemeMove: {...LightThemeMove, ...ResponsiveTheme},
-    DarkTheme: {...DarkTheme, ...ResponsiveTheme},
-    DarkThemeMove: {...DarkThemeMove, ...ResponsiveTheme},
+  LightTheme: { ...LightTheme, ...ResponsiveTheme },
+  LightThemeMove: { ...LightThemeMove, ...ResponsiveTheme },
+  DarkTheme: { ...DarkTheme, ...ResponsiveTheme },
+  DarkThemeMove: { ...DarkThemeMove, ...ResponsiveTheme },
 };
 
 export const themedStyled = createThemedStyled();
 export const themedWithStyle = createThemedWithStyle();
 export const themedUseStyletron = createThemedUseStyletron();
 
-const DARK_MEDIA_QUERY = '(prefers-color-scheme: dark)';
-const LIGHT_MEDIA_QUERY = '(prefers-color-scheme: light)';
+const DARK_MEDIA_QUERY = "(prefers-color-scheme: dark)";
+const LIGHT_MEDIA_QUERY = "(prefers-color-scheme: light)";
 
 const blockProps = {
-    color: 'contentPrimary',
-    backgroundColor: 'backgroundPrimary',
-    maxWidth: '100vw',
-    minHeight: '100vh',
-    overflow: 'hidden',
+  color: "contentPrimary",
+  backgroundColor: "backgroundPrimary",
+  maxWidth: "100vw",
+  minHeight: "100vh",
+  overflow: "hidden",
 };
 
 class MyApp extends App {
-    constructor(props) {
-        super(props);
-        this.state = {
-            theme: themes.LightTheme,
-            direction: 'ltr',
-        };
-        // $FlowFixMe
-        this.mediaQueryListener = this.mediaQueryListener.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      theme: themes.LightTheme,
+      direction: "ltr",
+    };
+    // $FlowFixMe
+    this.mediaQueryListener = this.mediaQueryListener.bind(this);
+  }
+
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
 
-    static async getInitialProps({ Component, ctx}) {
-        let pageProps = {};
-        if (Component.getInitialProps) {
-            pageProps = await Component.getInitialProps(ctx);
-        }
-
-        return {path: ctx.asPath, pageProps};
+    return { path: ctx.asPath, pageProps };
+  }
+  componentDidMount() {
+    Router.onRouteChangeComplete = (url) => {
+      trackPageView(url.split("?")[0]);
+    };
+    if (window.matchMedia) {
+      const mmDark = window.matchMedia(DARK_MEDIA_QUERY);
+      const mmLight = window.matchMedia(LIGHT_MEDIA_QUERY);
+      // if no theme is set in localStorage, set theme based on user's OS preference
+      if (!this.getThemeStyle() && mmDark.media === DARK_MEDIA_QUERY) {
+        const theme = mmDark.matches ? "dark" : "light";
+        localStorage.setItem("docs-theme", theme);
+      }
+      mmDark.addListener(this.mediaQueryListener);
+      mmLight.addListener(this.mediaQueryListener);
     }
-    componentDidMount() {
-        Router.onRouteChangeComplete = url => {
-            trackPageView(url.split('?')[0]);
-        };
-        if (window.matchMedia) {
-            const mmDark = window.matchMedia(DARK_MEDIA_QUERY);
-            const mmLight = window.matchMedia(LIGHT_MEDIA_QUERY);
-            // if no theme is set in localStorage, set theme based on user's OS preference
-            if (!this.getThemeStyle() && mmDark.media === DARK_MEDIA_QUERY) {
-                const theme = mmDark.matches ? 'dark' : 'light';
-                localStorage.setItem('docs-theme', theme);
-            }
-            mmDark.addListener(this.mediaQueryListener);
-            mmLight.addListener(this.mediaQueryListener);
-        }
-        this.setTheme();
+    this.setTheme();
+  }
+
+  componentWillUnmount() {
+    if (window.matchMedia) {
+      const mmDark = window.matchMedia(DARK_MEDIA_QUERY);
+      const mmLight = window.matchMedia(LIGHT_MEDIA_QUERY);
+      mmDark.removeListener(this.mediaQueryListener);
+      mmLight.removeListener(this.mediaQueryListener);
     }
+  }
 
-    componentWillUnmount() {
-        if (window.matchMedia) {
-            const mmDark = window.matchMedia(DARK_MEDIA_QUERY);
-            const mmLight = window.matchMedia(LIGHT_MEDIA_QUERY);
-            mmDark.removeListener(this.mediaQueryListener);
-            mmLight.removeListener(this.mediaQueryListener);
-        }
+  mediaQueryListener(e) {
+    if (e && e.matches) {
+      if (e.media === DARK_MEDIA_QUERY) {
+        this.setThemeStyle("dark");
+      } else if (e.media === LIGHT_MEDIA_QUERY) {
+        this.setThemeStyle("light");
+      }
+      this.setTheme();
     }
+  }
 
-    mediaQueryListener(e) {
-        if (e && e.matches) {
-            if (e.media === DARK_MEDIA_QUERY) {
-                this.setThemeStyle('dark');
-            } else if (e.media === LIGHT_MEDIA_QUERY) {
-                this.setThemeStyle('light');
-            }
-            this.setTheme();
-        }
-    }
+  setTheme() {
+    const search = window.location.search;
+    const ls = window.localStorage;
 
-    setTheme() {
-        const search = window.location.search;
-        const ls = window.localStorage;
+    const config = {
+      font: "system",
+      theme: "light",
+    };
 
-        const config = {
-            font: 'system',
-            theme: 'light',
-        };
+    const presetFont = ls.getItem("docs-font");
+    const presetTheme = ls.getItem("docs-theme");
 
-        const presetFont = ls.getItem('docs-font');
-        const presetTheme = ls.getItem('docs-theme');
+    let fontToSet;
+    let themeToSet;
 
-        let fontToSet;
-        let themeToSet;
-
-        if (search.includes('font=move')) {
-            fontToSet = 'move';
-            ls.setItem('docs-font', fontToSet);
-        }
-
-        if (search.includes('font=system')) {
-            fontToSet = 'system';
-            ls.setItem('docs-font', fontToSet);
-        }
-
-        if (search.includes('theme=dark')) {
-            themeToSet = 'dark';
-            ls.setItem('docs-theme', themeToSet);
-        }
-
-        if (search.includes('theme=light')) {
-            themeToSet = 'light';
-            ls.setItem('docs-theme', themeToSet);
-        }
-
-        config.font = fontToSet || presetFont || config.font;
-        config.theme = themeToSet || presetTheme || config.theme;
-
-        const themeName =
-            config.theme === 'dark'
-                ? config.font === 'move'
-                ? 'DarkThemeMove'
-                : 'DarkTheme'
-                : config.font === 'move'
-                ? 'LightThemeMove'
-                : 'LightTheme';
-
-        this.setState({
-            theme: themes[themeName],
-        });
+    if (search.includes("font=move")) {
+      fontToSet = "move";
+      ls.setItem("docs-font", fontToSet);
     }
 
-    getThemeStyle() {
-        return localStorage.getItem('docs-theme');
+    if (search.includes("font=system")) {
+      fontToSet = "system";
+      ls.setItem("docs-font", fontToSet);
     }
 
-    setThemeStyle(theme) {
-        localStorage.setItem('docs-theme', theme);
+    if (search.includes("theme=dark")) {
+      themeToSet = "dark";
+      ls.setItem("docs-theme", themeToSet);
     }
 
-    toggleTheme() {
-        const theme = this.getThemeStyle();
-
-        if (theme === 'dark') {
-            this.setThemeStyle('light');
-        } else {
-            this.setThemeStyle('dark');
-        }
-
-        this.setTheme();
+    if (search.includes("theme=light")) {
+      themeToSet = "light";
+      ls.setItem("docs-theme", themeToSet);
     }
 
-    toggleDirection() {
-        if (this.state.direction === 'rtl') {
-            this.setState({
-                direction: 'ltr',
-                theme: {...this.state.theme, direction: 'ltr'},
-            });
-            if (document.body) {
-                document.body.dir = 'ltr';
-            }
-        } else {
-            this.setState({
-                direction: 'rtl',
-                theme: {...this.state.theme, direction: 'rtl'},
-            });
-            if (document.body) {
-                document.body.dir = 'rtl';
-            }
-        }
+    config.font = fontToSet || presetFont || config.font;
+    config.theme = themeToSet || presetTheme || config.theme;
+
+    const themeName =
+      config.theme === "dark"
+        ? config.font === "move"
+          ? "DarkThemeMove"
+          : "DarkTheme"
+        : config.font === "move"
+        ? "LightThemeMove"
+        : "LightTheme";
+
+    this.setState({
+      theme: themes[themeName],
+    });
+  }
+
+  getThemeStyle() {
+    return localStorage.getItem("docs-theme");
+  }
+
+  setThemeStyle(theme) {
+    localStorage.setItem("docs-theme", theme);
+  }
+
+  toggleTheme() {
+    const theme = this.getThemeStyle();
+
+    if (theme === "dark") {
+      this.setThemeStyle("light");
+    } else {
+      this.setThemeStyle("dark");
     }
 
-    render(
-        const {Component, pageProps, path} = this.props;
-        return (
-            <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
-                <BaseProvider theme={this.state.theme}>
-                    <Block {...blockProps}>
-                        <DirectionContext.Provider value={this.state.direction}>
-                            <Component
-                                {...pageProps}
-                                path={path}
-                                toggleTheme={this.toggleTheme.bind(this)}
-                                toggleDirection={this.toggleDirection.bind(this)}
-                            />
-                        </DirectionContext.Provider>
-                    </Block>
-                </BaseProvider>
-            </StyletronProvider>
-        );
+    this.setTheme();
+  }
+
+  toggleDirection() {
+    if (this.state.direction === "rtl") {
+      this.setState({
+        direction: "ltr",
+        theme: { ...this.state.theme, direction: "ltr" },
+      });
+      if (document.body) {
+        document.body.dir = "ltr";
+      }
+    } else {
+      this.setState({
+        direction: "rtl",
+        theme: { ...this.state.theme, direction: "rtl" },
+      });
+      if (document.body) {
+        document.body.dir = "rtl";
+      }
     }
+  }
+  render() {
+    const { Component, pageProps, path } = this.props;
+    return (
+      <StyletronProvider value={styletron} debug={debug} debugAfterHydration>
+        <BaseProvider theme={this.state.theme}>
+          <Block {...blockProps}>
+            <DirectionContext.Provider value={this.state.direction}>
+              <Component
+                {...pageProps}
+                path={path}
+                toggleTheme={this.toggleTheme.bind(this)}
+                toggleDirection={this.toggleDirection.bind(this)}
+              />
+            </DirectionContext.Provider>
+          </Block>
+        </BaseProvider>
+      </StyletronProvider>
+    );
+  }
 }
 
-export default  appWithTranslation(MyApp);
+export default appWithTranslation(MyApp);
